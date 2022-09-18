d4139da08b0cd93356dc87bd1f6ac15eddbd45f9
Yury Benesh <yury.benesh@yandex.by>
2022-09-18 21:23:42 +0300

    read usage

--- invtool/src/lib.rs
+++ invtool/src/lib.rs
@@ -1,12 +1,11 @@
 // TODO check id to be > than the previous
-// TODO skip header
 use serde::Deserialize;
 use chrono::NaiveDate;
 
 #[derive(Debug, Deserialize)]
 pub struct Inventory {
     pub id: i32,
-    pub expiry: NaiveDate,
+    pub expiry_date: NaiveDate,
     pub name: String,
     pub amount: i32,
     pub record_date: NaiveDate,
@@ -14,16 +13,23 @@ pub struct Inventory {
     pub notes: String,
 }
 
+#[derive(Debug, Deserialize)]
+pub struct InventoryUsage {
+    pub inventory_id: i32,
+    pub open_date: NaiveDate,
+    pub new_expiry_date: NaiveDate,
+    pub record_date: NaiveDate,
+    pub notes: String,
+}
+
 
 #[cfg(test)]
 mod tests {
-    //use super::*;
-
-    use crate::Inventory;
+    use super::*;
 
     #[test]
     fn read_inventory() {
-        const ROWS:&str = "id,expiry,name,amount,record_date,origin,notes
+        const ROWS:&str = "id,expiry_date,name,amount,record_date,origin,notes
 1,2025-01-01,eye drops A,1,2022-09-18,City A,
 2,2025-01-01,eye drops B,1,2022-09-18,City A,";
 
@@ -34,6 +40,18 @@ mod tests {
             assert!(prev_id < record.id);
             assert_eq!(record.origin, "City A");
             prev_id = record.id;
+            println!("{:?}", &record);
+        }
+    }
+
+    #[test]
+    fn read_usage() {
+        const ROWS:&str = "inventory_id,open_date,new_expiry_date,record_date,notes
+1,2022-09-18,2022-09-25,2022-09-19,not sure of date";
+        let mut rdr = csv::Reader::from_reader(ROWS.as_bytes());
+        for result in rdr.deserialize() {
+            let record: InventoryUsage = result.unwrap();
+            assert_eq!(record.open_date, NaiveDate::from_ymd(2022, 09, 18));
             println!("{:?}", &record);
         }
     }
